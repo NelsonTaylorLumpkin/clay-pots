3a7b1fb9b2581c52d16661973aa945d90ea70c0e
Nelson Lumpkin <ossocasa@gmail.com>
2023-03-20 14:48:26 -0500

    first commit

---
+++ clay-pots.js
@@ -0,0 +1,10 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const cup of clay) {
+   const mug = `${cup} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+
+console.log(toFireInKiln)
